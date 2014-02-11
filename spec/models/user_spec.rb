9503e95d586c45a5b5e7344ff4c2f546c69cf8e3
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-11 00:33:13 -0500

    added validation to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,11 @@ describe User do
 		it { have_many :comments}
 	end
 
+	it "is invalid without an email" do
+		expect(User.create(password: 'asdfasdf')).to_not be_valid
+	end
+
+	it "is invalid without a password" do
+		expect(User.create(email: 'sue@email.com')).to_not be_valid
+	end
 end
