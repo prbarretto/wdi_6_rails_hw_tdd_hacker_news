9b7418f34da39135f7c060a4b5473fb0f38bb2af
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-10 23:34:36 -0500

    ran Articles model migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211042704) do
+ActiveRecord::Schema.define(version: 20140211043321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: true do |t|
+    t.text     "description"
+    t.text     "link"
+    t.integer  "votes"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
